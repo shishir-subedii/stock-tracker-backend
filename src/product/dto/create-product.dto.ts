6e969aaa0ee2f5dcc896a928179e0038f44e5621
Shishir <shishir16subedi@gmail.com>
2025-07-31 13:10:33 +0545

    Fixed product

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,32 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsString, IsNumber, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProductDto {
+    @ApiProperty({ example: 'Laptop' })
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @ApiProperty({ example: 'SKU12345' })
+    @IsString()
+    @IsNotEmpty()
+    sku: string;
+
+    @ApiProperty({ example: 1000 })
+    @IsNumber()
+    @Min(0)
+    price: number;
+
+    @ApiProperty({ example: 10 })
+    @IsNumber()
+    @Min(0)
+    initialStock: number;
+
+    @ApiProperty({ example: 5 })
+    @IsNumber()
+    reorderLevel: number;
+
+    @ApiProperty({ example: '1234-abcd' })
+    @IsString()
+    companyId: string;
+}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,28 +7,4 @@ import { UpdateProductDto } from './dto/update-product.dto';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.productService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(+id, updateProductDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
-  }
 }
